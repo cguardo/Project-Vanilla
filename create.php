8a126aaeeea5f741e531b59a7463e7549ea68d8e
vanessa <leoperio.vanessa@ue.edu.ph>
2022-12-10 23:00:26 +0800

    create and update record

--- create.php
+++ create.php
@@ -74,52 +74,106 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>Create Record</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-    .wrapper {
-        width: 600px;
-        margin: 0 auto;
-    }
-    </style>
+<title>Login V18</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
 
-<body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="mt-5">Create Record</h2>
+<body style="background-color: #666666;">
+<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login1001">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+					<span class="login100-form-title p-b-43">
+                    Create Record
+					</span>
                     <p>Please fill this form and submit to add employee record to the database.</p>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="form-group">
-                            <label>Name</label>
-                            <input type="text" name="name"
-                                class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>"
-                                value="<?php echo $name; ?>">
-                            <span class="invalid-feedback"><?php echo $name_err; ?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>Address</label>
-                            <textarea name="address"
-                                class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
-                            <span class="invalid-feedback"><?php echo $address_err; ?></span>
-                        </div>
-                        <div class="form-group">
-                            <label>Expiration Date</label>
-                            <input type="date" name="date"
-                                class="form-control <?php echo (!empty($date_err)) ? 'is-invalid' : ''; ?>"
-                                value="<?php echo $date; ?>">
-                            <span class="invalid-feedback"><?php echo $date_err; ?></span>
-                        </div>
+
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter a valid name">
+						<input type="text" name="name" class="input100 <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Name</span>
+						<span class="invalid-feedback"><?php echo $name_err; ?></span>
+					</div>
+
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter a valid address">
+						<input type="text" name="address" class="input100 <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?>
+						<span class="focus-input100"></span>
+						<span class="label-input100">Address</span>
+                        <span class="invalid-feedback"><?php echo $address_err; ?></span>
+					</div>
+
+                    <div class="wrap-input100 validate-input" data-validate="date is required">
+						<input type="date" name="date" class="input100 <?php echo (!empty($salary_err)) ? 'is-invalid' : ''; ?>"value="<?php echo $salary; ?>">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Expiration Date</span>
+						<span class="invalid-feedback"><?php echo $salary_err; ?></span>
+					</div>
+
+					<div class="flex-sb-m w-full p-t-3 p-b-32">
+						<!--<div class="contact100-form-checkbox">
+							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+							<label class="label-checkbox100" for="ckb1">
+								Remember me
+							</label>
+						</div>-->
+
+					</div>
+
+
+                    <input type="hidden" name="id" value="<?php echo $id; ?>" />
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+
+
+				</form>
+
+				<!--<div class="login100-more" style="background-image: url('images/bg-01.jpg');">
+				</div> -->
+			</div>
+		</div>
+	</div>
+
+ <!--===============================================================================================-->
+ <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>   
 </body>
 
 </html>
